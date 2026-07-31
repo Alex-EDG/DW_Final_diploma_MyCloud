bf26321d1137c90c42f83ebab9411322735423ca
Alex-EDG <AlexEDG75@GMail.Com>
2026-07-31 22:17:34 +0300

    Update file

--- backend/storage/management/commands/create_superuser.py
+++ backend/storage/management/commands/create_superuser.py
@@ -1,13 +1,13 @@
 import os
 from django.core.management import BaseCommand
-from users.models import User
+from storage.models import User
 
 
 class Command(BaseCommand):
     help = 'Создайте суперпользователя, если его ещё нет'
-    username = os.getenv('ADMIN_USERNAME', 'admin')
 
     def handle(self, *args, **options):
+        username = os.getenv('ADMIN_USERNAME', 'admin')
         password = os.getenv('ADMIN_PASSWORD', 'Admin123!')
         first_name = os.getenv('ADMIN_FIRSTNAME', 'Admin')
         last_name = os.getenv('ADMIN_LASTNAME', 'Administrator')
@@ -16,11 +16,25 @@ class Command(BaseCommand):
         if User.objects.filter(username=username).exists():
             self.stdout.write(self.style.WARNING(f'Суперпользователь "{username}" уже существует'))
         else:
-            User.objects.create_superuser(
+            admin_user = User.objects.create_user(
                 username=username,
-                email=email,
                 password=password,
                 first_name=first_name,
                 last_name=last_name,
+                email=email,
+                is_admin=True,
+                is_staff=True,      # Для доступа к Django админке
+                is_superuser=True   # Полные права в Django
             )
-            self.stdout.write(self.style.SUCCESS(f'Суперпользователь "{username}" успешно создан'))
+
+        self.stdout.write(
+            self.style.SUCCESS(
+                f'Администратор создан:\n'
+                f'Username: {admin_user.username}\n'
+                f'Password: {admin_user.password}\n'
+                f'First_name: {admin_user.first_name}\n'
+                f'Last_name: {admin_user.last_name}\n'
+                f'Email: {admin_user.email}\n'
+                f'Storage path: {admin_user.storage_path}'
+            )
+        )
